1014fa057db6adcbd20f015715426baeeb72810b
SOTA <snssota@gmail.com>
2022-09-24 21:43:48 +0900

    【リファクタリング】ヘッダー部分のコードを分離

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
 import "./App.css";
 import "./index.scss";
 import { Button } from "@carbon/react";
-import { Search, Notification, Switcher } from "@carbon/icons-react";
-import {
-  Header,
-  HeaderName,
-  HeaderGlobalAction,
-  HeaderGlobalBar,
-  HeaderNavigation,
-  HeaderMenu,
-  HeaderMenuItem,
-  Theme,
-} from "@carbon/react";
+import HeadBar from "./header.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Theme theme="g100">
-        <Header aria-label="IBM Platform Name">
-          <HeaderName href="#" prefix="Akitsu-Lab">
-            [Platform]
-          </HeaderName>
-          <HeaderNavigation aria-label="IBM [Platform]">
-            <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
-            <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-            <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
-            <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
-              <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
-              <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
-              <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
-            </HeaderMenu>
-          </HeaderNavigation>
-          <HeaderGlobalBar>
-            <HeaderGlobalAction aria-label="Search" onClick={() => {}}>
-              <Search />
-            </HeaderGlobalAction>
-            <HeaderGlobalAction aria-label="Notifications" onClick={() => {}}>
-              <Notification />
-            </HeaderGlobalAction>
-            <HeaderGlobalAction aria-label="App Switcher" onClick={() => {}}>
-              <Switcher />
-            </HeaderGlobalAction>
-          </HeaderGlobalBar>
-        </Header>
-      </Theme>
+      <HeadBar />
       <header className="App-header">
         <div>
           Hello Carbon! Well, not quite yet. This is the starting point for the
--- src/header.jsx
+++ src/header.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Search, Notification, Switcher } from "@carbon/icons-react";
 import {
   Header,
   HeaderName,
@@ -8,3 +10,37 @@ import {
   HeaderMenuItem,
   Theme,
 } from "@carbon/react";
+
+function HeadBar() {
+  return (
+    <Theme theme="g100">
+      <Header aria-label="IBM Platform Name">
+        <HeaderName href="#" prefix="Akitsu-Lab">
+          [Platform]
+        </HeaderName>
+        <HeaderNavigation aria-label="IBM [Platform]">
+          <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+          <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+          <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+          <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+            <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+            <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+            <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+          </HeaderMenu>
+        </HeaderNavigation>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction aria-label="Search" onClick={() => {}}>
+            <Search />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="Notifications" onClick={() => {}}>
+            <Notification />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher" onClick={() => {}}>
+            <Switcher />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    </Theme>
+  );
+}
+export default HeadBar;
