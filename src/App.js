9317f4174267d013b76bf2e2beed300ea66cb8cc
a163236 <nagakeita12@gmail.com>
2022-09-24 20:22:09 +0900

    change color of background and sentence

--- src/App.js
+++ src/App.js
@@ -7,23 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <div>
           Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
           tutorial.
         </div>
         <Button>Button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
