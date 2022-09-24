ebdb431800d2beddce824205e0c7c5e6c13c571b
SOTA <snssota@gmail.com>
2022-09-24 15:57:13 +0900

    環境構築

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.scss';
+import { Button } from '@carbon/react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
+          tutorial.
+        </div>
+        <Button>Button</Button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
