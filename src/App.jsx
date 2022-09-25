e1a72a454b320d8841b43b34e1892726249ff664
SOTA <snssota@gmail.com>
2022-09-25 20:08:29 +0900

    bookListでそれぞれのタイトルを取得

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import axios from "axios";
 
 export const App = () => {
 
-  const initialURL = "http://localhost:8080/services/v1/books/1";
+  const initialURL = "http://localhost:8080/services/v1/books";
+  // const initialURL = "http://localhost:8080/services/v1/books/1";
   // const initialURL = "http://os3-285-32121.vs.sakura.ne.jp:8080/services/v1/books";
   const [books, setBooks] = useState([]);
 
@@ -29,7 +30,13 @@ export const App = () => {
       <body className="App-body">
         🐒
         <BookList />
-        {books.bookTitle}
+        {/* {books.bookId}  <br />
+        {books.bookTitle} <br />
+        {books.bookPages} <br />
+        {books.bookContent} <br /> */}
+        {books.bookList[0].bookTitle} <br />
+        {books.bookList[1].bookTitle} <br />
+        {books.bookList[2].bookTitle} <br />
       </body>
       <footer>
 
