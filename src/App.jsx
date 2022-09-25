71e0f4b4e45920e9c5692f1f2b91e782359e6cc9
SOTA <snssota@gmail.com>
2022-09-25 17:11:09 +0900

    【リファクタリング】URL

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import axios from "axios";
 
 export const App = () => {
 
-  const initialURL = "http://localhost:8080/services/v1/books"; //"http://os3-285-32121.vs.sakura.ne.jp:8080/services/v1/books"
-
+  const initialURL = "http://localhost:8080/services/v1/books";
+  // const initialURL = "http://os3-285-32121.vs.sakura.ne.jp:8080/services/v1/books";
   useEffect(() => {
     console.log("booklistを取得します");
     axios.get(initialURL).then((res) => {
