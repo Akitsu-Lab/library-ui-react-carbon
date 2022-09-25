da1f5d161ef6de0972b8e43b646dc0952e524165
SOTA <snssota@gmail.com>
2022-09-25 18:50:43 +0900

    booksの１番目取得成功

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import "./App.css";
 import "./index.scss";
 import { HeadBar } from "./header.jsx";
 import { BookList } from "./booklistUI";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export const App = () => {
 
-  const initialURL = "http://localhost:8080/services/v1/books";
+  const initialURL = "http://localhost:8080/services/v1/books/1";
   // const initialURL = "http://os3-285-32121.vs.sakura.ne.jp:8080/services/v1/books";
+  const [books, setBooks] = useState([]);
+
   useEffect(() => {
     console.log("booklistを取得します");
     axios.get(initialURL).then((res) => {
       // setBooklist(res.data);
       console.log(res.data);
+      setBooks(res.data);
       if (res.status !== 200) { throw new Error("APIがうまく動作していないようです") }
     })
-  })
+  }, [])
 
   return (
     <div className="App">
@@ -26,6 +29,7 @@ export const App = () => {
       <body className="App-body">
         🐒
         <BookList />
+        {books.bookTitle}
       </body>
       <footer>
 
